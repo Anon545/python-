d116256303ce05980ede13d3c89894c4a97a1e42
Anon <73074342+Anon545@users.noreply.github.com>
2021-03-20 00:47:28 +0500

    Update Escaping.py

--- Escaping.py
+++ Escaping.py
@@ -1,27 +1,32 @@
-#escaping \
-# some_string0="i'm a aaaaaaaa"
-# print(some_string0)
-# another_string0='i want to learn "python'
-# print (another_string0)
-#
-# enter='\n'
-# print(enter)
-#
-# some_string='i\'m a aaaaaaaa'
-# print (some_string)
-# another_string="i want to learn \"python\""
-# print (another_string)
-
+#вывод текста двух переменных
+some_string0="i'm a aaaaaaaa"
+print(some_string0)
+another_string0='i want to learn "python'
+print (another_string0)
+#i'm a aaaaaaaa
+#i want to learn "python
+#два пустых абзаца для разрыва сплошной стены текста
 enter='\n'
 print(enter)
-
+#
+#escaping \ - чтобы можно было ставить кавычки в выводимом тексте.
+some_string='i\'m a aaaaaaaa'
+print (some_string)
+another_string="i want to learn \"python\""
+print (another_string)
+#i'm a aaaaaaaa ; i want to learn "python"
+enter='\n'
+print(enter)
+#
 string_with_new_line='helllo \n My name is A'
 print (string_with_new_line)
 
-string_with_new_line='helllo \n \rMy name is A'
+string_with_new_line='helllo \n \rMy name is A' #отсутствие отступа перед второй строкой
 print (string_with_new_line)
-
-numbers='1\t2345'
+#	Hello! 
+#	I'm aaaaa!
+#
+numbers='1\t2345'     #отступы Tab в строке между символами
 print (numbers)
-abzac="\tHello! \n\tI'm aaaaa!"
+abzac="\tHello! \n\tI'm aaaaa!"    #отступы и выравнивание отступов относительно друг друга
 print (abzac)
