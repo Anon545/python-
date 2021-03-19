8c2f03ad314656177254ce7d33ed37b5b6db0971
Anon <73074342+Anon545@users.noreply.github.com>
2021-03-20 00:38:03 +0500

    Add files via upload

---
+++ Escaping.py
@@ -0,0 +1,27 @@
+#escaping \
+# some_string0="i'm a aaaaaaaa"
+# print(some_string0)
+# another_string0='i want to learn "python'
+# print (another_string0)
+#
+# enter='\n'
+# print(enter)
+#
+# some_string='i\'m a aaaaaaaa'
+# print (some_string)
+# another_string="i want to learn \"python\""
+# print (another_string)
+
+enter='\n'
+print(enter)
+
+string_with_new_line='helllo \n My name is A'
+print (string_with_new_line)
+
+string_with_new_line='helllo \n \rMy name is A'
+print (string_with_new_line)
+
+numbers='1\t2345'
+print (numbers)
+abzac="\tHello! \n\tI'm aaaaa!"
+print (abzac)
