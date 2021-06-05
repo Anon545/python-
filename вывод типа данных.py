f2c5b33bd9eeabf66535dd4d6b8c370fba6fcf28
Anon <73074342+Anon545@users.noreply.github.com>
2021-06-05 05:21:16 +0500

    Add files via upload

---
+++ вывод типа данных.py
@@ -0,0 +1,8 @@
+text = "asdd"
+number = 15
+flotnumb = 12.65
+b  = True
+print (type(text))
+print (type(number))
+print (type(flotnumb))
+print (type(b))
---
+++ преобразование в дробь.py
@@ -0,0 +1,6 @@
+x = input('введите от 1 до 10')
+intx = float(x)
+print (type(x), intx, type(intx))
+
+txt=str(intx)
+print(txt, type(txt))
